30403bde73d3048b4e8028231f0d6b9366d77324
Gabe Nunez <gabriel.c.nunez@gmail.com>
2023-06-24 12:43:44 -0700

    Create test for pin validation challenge

---
+++ lib/pin_validation.rb
@@ -0,0 +1,19 @@
+# ATM PIN Code Validation
+# ATM machines allow 4 or 6 digit PIN codes and 
+# PIN codes cannot contain anything but exactly 
+# 4 digits or exactly 6 digits. Your task is to 
+# create a function that takes a string and returns 
+# true if the PIN is valid and false if it's not.
+
+# Examples
+# is_valid_PIN("1234") ➞ true
+
+# is_valid_PIN("12345") ➞ false
+
+# is_valid_PIN("a234") ➞ false
+
+# is_valid_PIN("") ➞ false
+
+# Notes
+# Some test cases contain special characters.
+# Empty strings must return false.
---
+++ spec/pin_validation_spec.rb
@@ -0,0 +1,27 @@
+require 'rspec'
+require './lib/pin_validation'
+
+RSpec.describe 'ATM PIN code validation' do
+  it 'validates whether a given string a valid 4-digit code' do
+    expect(is_valid_PIN("1234")).to eq(true)
+    expect(is_valid_PIN("12345")).to eq(false)
+    expect(is_valid_PIN("a234")).to eq(false)
+    expect(is_valid_PIN("")).to eq(false)
+    expect(is_valid_PIN("%234")).to eq(false)
+    expect(is_valid_PIN("`234")).to eq(false)
+    expect(is_valid_PIN("@234")).to eq(false)
+    expect(is_valid_PIN("#234")).to eq(false)
+    expect(is_valid_PIN("$234")).to eq(false)
+    expect(is_valid_PIN("*234")).to eq(false)
+    expect(is_valid_PIN("5678")).to eq(false)
+    expect(is_valid_PIN("^234")).to eq(false)
+    expect(is_valid_PIN("(234")).to eq(false)
+    expect(is_valid_PIN(")234")).to eq(false)
+    expect(is_valid_PIN("123456")).to eq(true)
+    expect(is_valid_PIN("-234")).to eq(false)
+    expect(is_valid_PIN("_234")).to eq(false)
+    expect(is_valid_PIN("+234")).to eq(false)
+    expect(is_valid_PIN("=234")).to eq(false)
+    expect(is_valid_PIN("?234")).to eq(false)
+  end
+end
