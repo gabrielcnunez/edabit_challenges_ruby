74b241cc94acc42429d089d0f6f5ed6d0648342e
Gabe Nunez <gabriel.c.nunez@gmail.com>
2023-06-27 17:29:46 -0700

    Create test for check square and cube challenge

---
+++ lib/check_square_and_cube.rb
@@ -0,0 +1,15 @@
+# Squares and Cubes
+# Create a function that takes a list of two numbers 
+# and checks if the square root of the first number is 
+# equal to the cube root of the second number.
+
+# Examples
+# check_square_and_cube([4, 8]) ➞ true
+
+# check_square_and_cube([16, 48]) ➞ false
+
+# check_square_and_cube([9, 27]) ➞ true
+
+# Notes
+# Remember to return either true or false.
+# All lists contain two positive numbers.
---
+++ spec/check_square_and_cube_spec.rb
@@ -0,0 +1,17 @@
+require 'rspec'
+require './lib/check_square_and_cube'
+
+RSpec.describe 'check_square_and_cube' do
+  it 'takes two numbers to see if the square root of the first equals the cube root of the second' do
+    expect(check_square_and_cube([4, 8])).to eq(true)
+    expect(check_square_and_cube([5, 12])).to eq(false)
+    expect(check_square_and_cube([9, 27])).to eq(true)
+    expect(check_square_and_cube([25, 120])).to eq(false)
+    expect(check_square_and_cube([25, 125])).to eq(true)
+    expect(check_square_and_cube(36, 215)).to eq(false)
+    expect(check_square_and_cube([36, 217])).to eq(false)
+    expect(check_square_and_cube([144, 1728])).to eq(true)
+    expect(check_square_and_cube([1, 1])).to eq(true)
+    expect(check_square_and_cube([676, 17576])).to eq(true)
+  end 
+end
