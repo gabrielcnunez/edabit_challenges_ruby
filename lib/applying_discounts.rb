214f3967e30559de00c86e57820e173de2970101
Gabe Nunez <gabriel.c.nunez@gmail.com>
2023-07-08 23:44:57 -0700

    Clean up description of applying discounts challenge

--- lib/applying_discounts.rb
+++ lib/applying_discounts.rb
@@ -12,7 +12,7 @@
 # The discount is the percentage of the original price (i.e the discount 
 # of "75%" to 12 would be 9 as opposed to taking off 75% (making 3)).
 
-# There won't be any awkward decimal numbers, only 0.5 to deal with.\
+# There won't be any awkward decimal numbers, only 0.5 to deal with.
 
 def get_discounts(nums, d)
   nums.map { |num| num * (d.to_i * 0.01) }
