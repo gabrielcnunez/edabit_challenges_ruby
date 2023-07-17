ceafb4e607231aa85fb53c33d88cf0f67398f4dc
Gabe Nunez <gabriel.c.nunez@gmail.com>
2023-07-16 22:31:54 -0700

    Refactor can you make the numbers solution without creating hashes

--- lib/make_numbers.rb
+++ lib/make_numbers.rb
@@ -21,18 +21,35 @@
 # Notes
 # N/A
 
+# def can_build(digits, arr)
+#   return true if arr.empty?
+
+#   digit_quantity = {}
+#   (0..9).each do |i|
+#     digit_quantity[i] = digits[i]
+#   end
+#   array_quantity = arr.join.chars.tally
+
+#   array_quantity.each do |k,_|
+#     return false if array_quantity[k] > digit_quantity[k.to_i]
+#   end
+
+#   true
+# end
+
 def can_build(digits, arr)
   return true if arr.empty?
 
-  digit_quantity = {}
-  (0..9).each do |i|
-    digit_quantity[i] = digits[i]
-  end
-  array_quantity = arr.join.chars.tally
+  digits_available = digits.dup
 
-  array_quantity.each do |k,_|
-    return false if array_quantity[k] > digit_quantity[k.to_i]
+  arr.each do |num|
+    num.digits.each do |digit|
+      return false if digits_available[digit] <= 0
+
+      digits_available[digit] -= 1
+    end
   end
 
   true
 end
+
