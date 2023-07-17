1700b3ab9cfbb52334797fe707974003a2769a3a
Gabe Nunez <gabriel.c.nunez@gmail.com>
2023-07-16 22:21:21 -0700

    Solve can you make the numbers challenge

--- spec/make_numbers_spec.rb
+++ spec/make_numbers_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'Can you make the numbers?' do
   it 'returns boolean if array of single digit groups can make number(s)' do
     expect(can_build([0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [])).to eq(true)
     expect(can_build([1, 1, 0, 0, 0, 0, 0, 0, 1, 0], [1, 8])).to eq(true)
-    expect(can_build([0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [1, 80])).to eq(true)
+    expect(can_build([1, 1, 0, 0, 0, 0, 0, 0, 1, 0], [1, 80])).to eq(true)
     expect(can_build([0, 1, 2, 2, 3, 0, 0, 0, 1, 1], [123, 444, 92])).to eq(true)
     expect(can_build([1, 1, 1, 1, 1, 1, 1, 1, 1, 1], [10, 23, 45, 6789])).to eq(true)
     expect(can_build([0, 2, 3, 0, 5, 0, 0, 0, 0, 1], [11, 2, 22, 49, 444, 4])).to eq(true)
