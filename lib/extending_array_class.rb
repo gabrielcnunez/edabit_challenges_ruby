0811a2da74b0325ef13039df65c551e1727f5331
Gabe Nunez <gabriel.c.nunez@gmail.com>
2023-07-20 13:52:18 -0700

    Create test for extending the array class challenge

---
+++ lib/extending_array_class.rb
@@ -0,0 +1,22 @@
+# Extending the Array Class
+# Write the following functions to extend the array class, by adding the methods:
+
+# square()
+# cube()
+# divisible_by(x)
+# strictly_above(x)
+# strictly_below(x)
+
+# Examples
+# [1, 2, 3].square ➞ [1, 4, 9]
+
+# [1, 2, 3].cube ➞ [1, 8, 27]
+
+# [1, 2, 3, 4].divisible_by(2) ➞ [2, 4]
+
+# [1, 2, 3, 4].strictly_above(1) ➞ [2, 3, 4]
+
+# [1, 2, 3, 4].strictly_below(2) ➞ [1]
+
+# Notes
+# N/A
