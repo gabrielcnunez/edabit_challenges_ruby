31edb88b04d7f793a62a345ddc695aa05589ac26
Gabe Nunez <gabriel.c.nunez@gmail.com>
2023-07-22 14:30:01 -0700

    Solve highest pair challenge

--- lib/highest_pair.rb
+++ lib/highest_pair.rb
@@ -19,3 +19,29 @@
 # Notes
 # Hands with three or more of the same card still count as 
 # containing a pair (see the last example).
+
+def highest_pair(cards)
+  suit_order = {
+    "A" => 1,
+    "K" => 2,
+    "Q" => 3,
+    "J" => 4,
+    "10" => 5,
+    "9" => 6,
+    "8" => 7,
+    "7" => 8,
+    "6" => 9,
+    "5" => 10,
+    "4" => 11,
+    "3" => 12,
+    "2" => 13
+  }
+  pairs = []
+  cards.tally.each do |card, quantity|
+    if quantity > 1
+      pairs << card
+    end
+  end
+  
+  pairs.empty? ? false : [true, pairs.sort_by { |pair| suit_order[pair] }[0]]
+end
