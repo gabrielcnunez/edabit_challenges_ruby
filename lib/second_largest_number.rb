dd733d1da3c76624db18d1f446be63bef200e7d8
Gabe Nunez <gabriel.c.nunez@gmail.com>
2023-07-24 20:03:10 -0700

    Create test for find the second largest number challenge

---
+++ lib/second_largest_number.rb
@@ -0,0 +1,14 @@
+# Find the Second Largest Number
+# Create a function that takes an array of numbers and 
+# returns the second largest number.
+
+# Examples
+# second_largest([10, 40, 30, 20, 50]) ➞ 40
+
+# second_largest([25, 143, 89, 13, 105]) ➞ 105
+
+# second_largest([54, 23, 11, 17, 10]) ➞ 23
+
+# Notes
+# There will be at least two numbers in the array.
+
---
+++ spec/second_largest_number_spec.rb
@@ -0,0 +1,12 @@
+require 'rspec'
+require './lib/second_largest_number'
+
+RSpec.describe 'Find the second largest number' do
+  it 'returns the second largest integer from an array in integers' do
+    expect(second_largest([10, 40, 30, 20, 50])).to eq(40)
+    expect(second_largest([25, 143, 89, 13, 105])).to eq(105)
+    expect(second_largest([54, 23, 11, 17, 10])).to eq(23)
+    expect(second_largest([513, 211, 131, 417, 11])).to eq(41)
+    expect(second_largest([63, 44, 11, 22, 33, 66, 65])).to eq(65)
+  end
+end
