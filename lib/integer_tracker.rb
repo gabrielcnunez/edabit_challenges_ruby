155585034e3fa79c22d0ebe8271a0333a901d276
Gabe Nunez <gabriel.c.nunez@gmail.com>
2023-07-25 22:30:46 -0700

    Solve integer tracker challenge

--- lib/integer_tracker.rb
+++ lib/integer_tracker.rb
@@ -9,21 +9,52 @@
 #
 # Each funtion must be O(1) / constant time complexity.
 
+class IntTracker
+  attr_reader :min, :max, :mode
+  def initialize
+    @ints = Hash.new(0)
+    @total = 0
+    @min = nil
+    @max = nil
+    @mode = nil
+    @count = 0
+  end
+
+  def track(input)
+    @count += 1
+    @ints[input] += 1
+    @total += input
+    if @min.nil? || input < @min
+      @min = input
+    end
+    if @max.nil? || input > @max
+      @max = input
+    end
+    if @mode.nil? || @ints[input] > @mode
+      @mode = input
+    end
+  end
+
+  def mean
+    @total.to_f / @count
+  end
+end
+
 tracker = IntTracker.new
-​
+
 input = [2, 4, 10, 1, 4, 1, 1, 1]
 input.each do |num|
   tracker.track(num)
 end
-​
+
 p tracker.mean
 # => 3.0
-​
+
 p tracker.min
 # => 1
-​
+
 p tracker.max
 # => 10
-​
+
 p tracker.mode
 # => 1
