291c5e099b1768434f7f580bf2463bd710f22e13
Gabe Nunez <gabriel.c.nunez@gmail.com>
2023-08-17 11:25:53 -0700

    Create test for recursion: reverse a string

---
+++ lib/recursion_reverse_string.rb
@@ -0,0 +1,15 @@
+# Recursion: Reverse a String
+# Write a function that reverses a string. Make your function recursive.
+
+# Examples
+# reverse("hello") ➞ "olleh"
+
+# reverse("world") ➞ "dlrow"
+
+# reverse("a") ➞ "a"
+
+# reverse("") ➞ ""
+
+# Notes
+# For non-base cases, your function must call itself at least once.
+# Check the Resources tab for info on recursion.
---
+++ spec/recursion_reverse_string_spec.rb
@@ -0,0 +1,23 @@
+require 'rspec'
+require './lib/recursion_reverse_string'
+
+RSpec.describe 'Recursion: Reverse a String' do
+  it 'returns a strings reversed using recursion' do
+    count = 0
+    name = :reverse 
+
+    TracePoint.trace(:call) do |t|
+      count += 1 if t.method_id == name 
+    end
+
+    expect(reverse("hello")).to eq("olleh")
+    expect(count).to_not eq(1, "Function needs to be called more than once.")
+    
+    count = 0
+    expect(reverse("world")).to eq("dlrow")
+    expect(count).to_not eq(1, "Function needs to be called more than once.")
+  
+    expect(reverse("a")).to eq("a")
+    expect(reverse("")).to eq("", "An empty string should return an empty string.")
+  end
+end
