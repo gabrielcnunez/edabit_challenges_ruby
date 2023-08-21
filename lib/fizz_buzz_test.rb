e048cc518d7904d44eb59f61956c4ddc8c69ae69
Gabe Nunez <gabriel.c.nunez@gmail.com>
2023-08-21 14:08:38 -0700

    Solve the fizz buzz test challenge

--- lib/fizz_buzz_test.rb
+++ lib/fizz_buzz_test.rb
@@ -10,3 +10,17 @@
 
 # Notes
 # Make sure to return an array.
+
+def fizz_buzz(int)
+  (1..int).map do |i|
+    if i % 15 == 0
+      "FizzBuzz"
+    elsif i % 5 == 0
+      "Buzz"
+    elsif i % 3 == 0
+      "Fizz"
+    else
+      i
+    end
+  end
+end
