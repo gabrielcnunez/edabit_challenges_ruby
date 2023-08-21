c6ba313ae8df52ce83e7b20cb2918e6b2c54851c
Gabe Nunez <gabriel.c.nunez@gmail.com>
2023-08-21 14:01:36 -0700

    Create test for the fizz buzz test challenge

---
+++ lib/fizz_buzz_test.rb
@@ -0,0 +1,12 @@
+# The Fizz Buzz Test
+# Write a program that returns an array of all the numbers from 1 to an integer argument. But for multiples 
+# of three use “Fizz” instead of the number and for the multiples of five use “Buzz”. For numbers which are 
+# multiples of both three and five use “FizzBuzz”.
+
+# Example
+# fizz_buzz(10) ➞ [1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz"]
+
+# fizz_buzz(15) ➞ [1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz", 11, "Fizz", 13, 14, "FizzBuzz"]
+
+# Notes
+# Make sure to return an array.
