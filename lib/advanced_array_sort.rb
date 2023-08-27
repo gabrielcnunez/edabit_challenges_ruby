0f38d3d0b962967d6d775c20a7fe5e20cdcd032c
Gabe Nunez <gabriel.c.nunez@gmail.com>
2023-08-27 16:26:53 -0700

    Create test for advanced array sort challenge

---
+++ lib/advanced_array_sort.rb
@@ -0,0 +1,13 @@
+# Advanced Array Sort
+# Create a function that takes an array of numbers or strings and returns an array 
+# with the items from the original array stored into subarrays. Items of the same value should be in the same subarray.
+
+# Examples
+# advanced_sort([2, 1, 2, 1]) ➞ [[2, 2], [1, 1]]
+
+# advanced_sort([5, 4, 5, 5, 4, 3]) ➞ [[5, 5, 5], [4, 4], [3]]
+
+# advanced_sort(["b", "a", "b", "a", "c"]) ➞ [["b", "b"], ["a", "a"], ["c"]]
+
+# Notes
+# The subarrays should be returned in the order of each element's first appearance in the given array.
---
+++ spec/advanced_array_sort_spec.rb
@@ -0,0 +1,15 @@
+require 'rspec'
+require './lib/advanced_array_sort'
+
+RSpec.describe 'Advanced array sort' do
+  it 'takes array of numbers or strings, returns them sorted into sub-arrays in order of appearance' do
+    expect(advanced_sort([1,2,1,2])).to eq([[1,1],[2,2]])
+    expect(advanced_sort([2,1,2,1])).to eq([[2,2],[1,1]])
+    expect(advanced_sort([3,2,1,3,2,1])).to eq([[3,3],[2,2],[1,1]])
+    expect(advanced_sort([5,5,4,3,4,4])).to eq([[5,5],[4,4,4],[3]])
+    expect(advanced_sort([80,80,4,60,60,3])).to eq([[80,80],[4],[60,60],[3]])
+    expect(advanced_sort(['c','c','b','c','b',1,1])).to eq([['c','c','c'],['b','b'],[1,1]])
+    expect(advanced_sort([1234, 1235, 1234, 1235, 1236, 1235])).to eq([[1234, 1234],[1235, 1235, 1235],[1236]])
+    expect(advanced_sort(['1234', '1235', '1234', '1235', '1236', '1235'])).to eq([['1234', '1234'],['1235', '1235', '1235'],['1236']])
+  end
+end
