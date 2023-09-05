a17fb18f5e4a7c2c3a820f2ce4b6f1903c6ec066
Gabe Nunez <gabriel.c.nunez@gmail.com>
2023-09-05 09:36:55 -0700

    Solve swapping two by two challenge

--- lib/swapping_two_by_two.rb
+++ lib/swapping_two_by_two.rb
@@ -14,3 +14,12 @@
 # Notes
 # Keep leftover strings in the same order.
 
+def swap_two(str)
+  char_swap = []
+
+  str.split('').each_slice(4) do |a|
+    a.size == 4 ? char_swap << a[2] << a[3] << a[0] << a[1] : char_swap << a
+  end
+
+  char_swap.join
+end
