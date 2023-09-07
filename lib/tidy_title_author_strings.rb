3d2d87bd51d42394c1a1396574e6f20497ce48ab
Gabe Nunez <gabriel.c.nunez@gmail.com>
2023-09-07 12:07:32 -0700

    Solve tidy title and author strings challenge

--- lib/tidy_title_author_strings.rb
+++ lib/tidy_title_author_strings.rb
@@ -35,3 +35,11 @@
 
 # Notes
 # Some of these entries have excess white space. Remove this white space in your final output.
+
+def tidy_books(arr)
+  arr.map do |e|
+    x = e[0].strip.split('-')
+
+    [x[0].rstrip, x[1].lstrip]
+  end
+end
