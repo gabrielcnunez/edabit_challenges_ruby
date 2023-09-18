923e4bd74b4847ac39b05e973cb3a10fe49c3f9c
Gabe Nunez <gabriel.c.nunez@gmail.com>
2023-09-18 12:51:43 -0700

    Refactor string incrementer solution using chatgpt method

--- lib/string_incrementer.rb
+++ lib/string_incrementer.rb
@@ -14,25 +14,39 @@
 # Notes
 # N/A
 
-def increment_string(str)
-  if str[-1].ord.between?(48, 57)
-    word = []
-    number = []
+# def increment_string(str)
+#   if str[-1].ord.between?(48, 57)
+#     word = []
+#     number = []
     
-    str.each_char { |char| char.ord.between?(48, 57) ? number << char : word << char}
+#     str.each_char { |char| char.ord.between?(48, 57) ? number << char : word << char}
     
-    number_index = number.index { |x| x != '0' }
-    unchanged_number = number[number_index..-1].join.to_i
-    changed_number = unchanged_number + 1
+#     number_index = number.index { |x| x != '0' }
+#     unchanged_number = number[number_index..-1].join.to_i
+#     changed_number = unchanged_number + 1
     
-    if changed_number.to_s.size == unchanged_number.to_s.size
-      number[number_index..-1] = changed_number.to_s.split('')
-    else
-      number[number_index - 1] == '0' ? number[number_index - 1..-1] = changed_number.to_s.split('') : (number[number_index - 1].to_i + 1).to_s
-    end
+#     if changed_number.to_s.size == unchanged_number.to_s.size
+#       number[number_index..-1] = changed_number.to_s.split('')
+#     else
+#       number[number_index - 1] == '0' ? number[number_index - 1..-1] = changed_number.to_s.split('') : (number[number_index - 1].to_i + 1).to_s
+#     end
 
-    (word + number).join
+#     (word + number).join
+#   else
+#     str + '1'
+#   end
+# end
+
+def increment_string(str)
+  match = str.match(/(\d+)$/)
+
+  if match
+    require 'pry'; binding.pry
+    number = match[1]
+    incremented_number = (number.to_i + 1).to_s
+ 
+    str.sub(/#{number}$/, incremented_number.rjust(number.length, '0'))
   else
-    str + '1'
+    str + "1"
   end
 end
