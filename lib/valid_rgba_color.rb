39e94e221bef3abb0ffd9292de64614b50ed2adb
Gabe Nunez <gabriel.c.nunez@gmail.com>
2023-09-24 17:11:49 -0700

    Solve valid rgba color challenge

--- lib/valid_rgba_color.rb
+++ lib/valid_rgba_color.rb
@@ -17,5 +17,26 @@
 # There are a few edge cases (check out the Tests tab to know more).
 
 def valid_color(str)
+  return false if str =~ /\s+\(/
   
+  nums_array = str[/\((.*?)\)/, 1].split(',')
+
+  return false if nums_array.include?('')
+  
+  colors_array = nums_array[0, 3].map(&:to_i)
+  alpha_value = nums_array[3]
+
+  return false if (str.include?('a') && !alpha_value) || (!str.include?('a') && alpha_value)
+
+  if str.include?('%')
+    return false if colors_array.any? { |i| i < 0 || i > 100 }
+  else
+    return false if colors_array.any? { |i| i < 0 || i > 255 }
+  end
+
+  if alpha_value 
+    return false if alpha_value.to_f < 0 || alpha_value.to_f > 1
+  end
+
+  true
 end
