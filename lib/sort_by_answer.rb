83d2755a21f98f9694fbd06fb68c9eb59412b735
Gabe Nunez <gabriel.c.nunez@gmail.com>
2023-09-29 09:53:36 -0700

    Create test for sort by answer challenge

---
+++ lib/sort_by_answer.rb
@@ -0,0 +1,19 @@
+# Sort by Answer
+# Given an array of math expressions, create a function which sorts the array by their answer. 
+# It should be sorted in ascending order.
+
+# Examples
+# sort_by_answer(["1 + 1", "1 + 7", "1 + 5", "1 + 4"]) ➞ ["1 + 1", "1 + 4", "1 + 5", "1 + 7"]
+
+# sort_by_answer(["4 - 4", "2 - 2", "5 - 5", "10 - 10"]) ➞ ["4 - 4", "2 - 2", "5 - 5", "10 - 10"]
+
+# sort_by_answer(["2 + 2", "2 - 2", "2 * 1"]) ➞ ["2 - 2", "2 * 1", "2 + 2"]
+
+# Notes
+# If multiple expressions have the same answer, put them in the order of which they appear (see example #2).
+# You won't need to worry about divisions by zero.
+# The symbol used for multiplication is x instead of *.
+
+def sort_by_answer(arr)
+  
+end
