b854d9da4682504e19e2f0eca6dede8c18d9aaef
Gabe Nunez <gabriel.c.nunez@gmail.com>
2023-10-01 15:54:47 -0700

    Refactor the hangman game solution to remove variable

--- lib/the_hangman_game.rb
+++ lib/the_hangman_game.rb
@@ -17,7 +17,7 @@
 # All characters other than letters should always be returned (see example #4).
 
 def hangman(str, arr)
-  x = str.chars.map do |char|
+  str.chars.map do |char|
     if char.match?(/[a-zA-Z]/)
       arr.include?(char.downcase) ? char : '-'
     else
