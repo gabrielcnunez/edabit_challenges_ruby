c4de0fb94235eb51de4fa4108a4fc25e8ed34d8a
Gabe Nunez <gabriel.c.nunez@gmail.com>
2023-10-04 17:38:45 -0700

    Create test for count the points in a circle challenge

---
+++ lib/count_points_in_circle.rb
@@ -0,0 +1,19 @@
+# Count the Points in a Circle
+# Count the total number of coordinates on a two-dimensional grid that are inside a given
+# circle. The function has four parameters: the points (provided as an array of hashes), 
+# the circle's center x coordinate, the circle's center y coordinate, and the circle's radius.
+
+# Examples
+# points_in_circle([
+#   { x: 0, y: 0 },
+#   { x: 1, y: 1 },
+#   { x: 0, y: 5 },
+#   { x: 10, y: 10 }
+# ], 0, 0, 5) ➞ 2
+
+# Notes
+# Only count the coordinates that are in the circle, not the ones that are on the border.
+
+def points_in_circle(points, centerX, centerY, radius)
+  
+end
