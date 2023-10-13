02cc32b1b1d2d70f9c12d9e827f32d2d250ab87b
Gabe Nunez <gabriel.c.nunez@gmail.com>
2023-10-12 19:53:26 -0700

    Solve superheroes challenge

--- lib/superheroes.rb
+++ lib/superheroes.rb
@@ -15,6 +15,22 @@
 # Notes
 # Wonder-Woman, Catwoman and Invisible-Woman are superheroines.
 
+# Annie
+# Iterate through array of strings, assess each name to see if last three characters in string contain 'man' and two characters before do not contain 'wo'
+# Watch out for capitalization, sort in alphabetical order using .sort
+
 def superheroes(arr)
+  superhero_array = []
+
+  arr.each do |name|
+    dc_name = name.downcase
+
+    superhero_array << name if dc_name[-3..-1] == 'man' && dc_name[-5..-4] != 'wo'
+  end
   
+  superhero_array.sort
 end
+
+# def superheroes(arr)
+#   arr.find_all { |name| name[-3..-1] == 'man' && name.downcase[-5..-4] != 'wo' }.sort
+# end
