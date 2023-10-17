b88f463d9e038ab7aac2130d83e8ee611f8cdd58
Gabe Nunez <gabriel.c.nunez@gmail.com>
2023-10-17 12:16:02 -0700

    Solve scrabble scores part one challenge

--- lib/scrabble_scores_part1.rb
+++ lib/scrabble_scores_part1.rb
@@ -41,5 +41,43 @@
 # Input and output should both be arrays (see tests).
 
 def best_words(arr)
-  
+  letter_scores = {
+    'a' => 1,
+    'b' => 3,
+    'c'	=> 3,
+    'd'	=> 2,
+    'e'	=> 1,
+    'f'	=> 4,
+    'g'	=> 2,
+    'h'	=> 4,
+    'i'	=> 1,
+    'j'	=> 8,
+    'k'	=> 5,
+    'l'	=> 2,
+    'm'	=> 3,
+    'n'	=> 1,
+    'o'	=> 1,
+    'p'	=> 3,
+    'q'	=> 10,
+    'r'	=> 1,
+    's'	=> 1,
+    't'	=> 1,
+    'u'	=> 1,
+    'v'	=> 4,
+    'w'	=> 4,
+    'x'	=> 8,
+    'y'	=> 4,
+    'z'	=> 10
+  }
+
+  highest_score = arr.map do |word|
+    sum = 0
+    word.each_char.sum { |char| sum += letter_scores[char] }
+    
+    sum
+  end.max
+
+  arr.find_all do |word|
+    word.each_char.map { |char| letter_scores[char] }.sum == highest_score
+  end
 end
