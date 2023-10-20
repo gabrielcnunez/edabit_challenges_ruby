d4fa9fbb43e6c1e0c9a8fe9b3cd888d965055145
Gabe Nunez <gabriel.c.nunez@gmail.com>
2023-10-19 17:31:54 -0700

    Create test for wrong number challenge

---
+++ lib/wrong_number.rb
@@ -0,0 +1,60 @@
+# Wrong Number
+# Mubashir needs your help to find out a wrong number in a 2D array.
+
+# Imagine a 2D array of numbers given below:
+
+# arr = [
+#   [1, 2, 3, 6 ],
+#   [4, 5, 6, 15],
+#   [7, 8, 9, 24],
+#   [12,15,18,45]
+# ]
+# You can notice that:
+
+# The end number of each row is the sum of each row's previous numbers.
+# The end number of each column is the sum of each column's previous numbers.
+# See below examples for a better understanding:
+
+# arr1 = [
+#   [2, 2, 3, 6 ],
+#   [4, 5, 6, 15],
+#   [7, 8, 9, 24],
+#   [12,15,18,45]
+# ]
+
+# # 2 is incorrect in first row and first column.
+# # Replace it with 1.
+
+# arr2 = [
+#   [1, 2, 3, 7 ],
+#   [4, 5, 6, 15],
+#   [7, 8, 9, 24],
+#   [12,15,18,45]
+# ]
+
+# # 7 is incorrect in first row and fourth column.
+# # Replace it with 6.
+
+# arr3 = [
+#   [1, 2, 3, 6 ],
+#   [4, 5, 6, 15],
+#   [7, 8, 9, 24],
+#   [12,15,18,46]
+# ]
+
+# # 46 is incorrect in fourth row and fourth column.
+# # Replace it with 45.
+
+# Examples
+# wrong_number(arr1) ➞ 1
+
+# wrong_number(arr2) ➞ 6
+
+# wrong_number(arr3) ➞ 45
+
+# Notes
+# N/A
+
+def wrong_number(arr)
+  
+end
---
+++ spec/wrong_number_spec.rb
@@ -0,0 +1,30 @@
+require 'rspec'
+require './lib/wrong_number'
+
+RSpec.describe 'Wrong Number' do
+  it 'takes 2D array of numbers, returns wrong number in array based on pattern' do
+    arr=[
+    [2, 2, 3, 6 ],
+    [4, 5, 6, 15],
+    [7, 8, 9, 24],
+    [12,15,18,45]
+    ]
+    expect(wrong_number(arr)).to eq(1)
+
+    arr=[
+    [1, 2, 3, 7 ],
+    [4, 5, 6, 15],
+    [7, 8, 9, 24],
+    [12,15,18,45]
+    ]
+    expect(wrong_number(arr)).to eq(6)
+
+    arr=[
+    [1, 2, 3, 6 ],
+    [4, 5, 6, 15],
+    [7, 8, 9, 24],
+    [12,15,18,46]
+    ]
+    expect(wrong_number(arr)).to eq(4)
+  end
+end
