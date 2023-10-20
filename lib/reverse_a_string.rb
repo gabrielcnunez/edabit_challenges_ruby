240403dcae01340f5b8ad906e6f834aecb9737eb
Gabe Nunez <gabriel.c.nunez@gmail.com>
2023-10-20 08:16:59 -0700

    Create test for reverse a string without using reverse challenge

---
+++ lib/reverse_a_string.rb
@@ -0,0 +1,17 @@
+# Reverse the Order of a String
+# Create a function that takes a string as its argument and returns 
+# the string in reversed order without using the built-in .reverse method.
+
+# Examples
+# reverse("Hello World") ➞ "dlroW olleH"
+
+# reverse("The quick brown fox.") ➞ ".xof nworb kciuq ehT"
+
+# reverse("Edabit is really helpful!") ➞ "!lufpleh yllaer si tibadE"
+
+# Notes
+# You can expect a valid string for all test cases.
+
+def reverse(str)
+  
+end
