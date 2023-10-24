2d208f91b2430fc79327015c6b3f7cd82f2d039a
Gabe Nunez <gabriel.c.nunez@gmail.com>
2023-10-24 13:49:31 -0700

    Create test for all occurrences of element in array challenge

---
+++ lib/all_occurrences.rb
@@ -0,0 +1,20 @@
+# All Occurrences of an Element in an Array
+# Create a function that returns the indices of all occurrences of an item in the array.
+
+# Examples
+# get_indices(["a", "a", "b", "a", "b", "a"], "a") ➞ [0, 1, 3, 5]
+
+# get_indices([1, 5, 5, 2, 7], 7) ➞ [4]
+
+# get_indices([1, 5, 5, 2, 7], 5) ➞ [1, 2]
+
+# get_indices([1, 5, 5, 2, 7], 8) ➞ []
+
+# Notes
+# If an element does not exist in an array, return [].
+# Arrays are zero-indexed.
+# Values in the array will be value-types (don't need to worry about nested arrays).
+
+def get_indices(arr, element)
+
+end
---
+++ spec/all_occurrences_spec.rb
@@ -0,0 +1,17 @@
+require 'rspec'
+require './lib/all_occurrences'
+
+# frozen_string_literal: true
+
+describe 'All Occurrences of an Element in an Array' do
+  it "returns indices of all occurrences of item in array" do
+    expect(get_indices(['a', 'a', 'b', 'a', 'b', 'a'], 'a')).to eq([0, 1, 3, 5])
+    expect(get_indices([1, 5, 5, 2, 7], 7)).to eq([4])
+    expect(get_indices([1, 5, 5, 2, 7], 5),[1, 2])
+    expect(get_indices([1, 5, 5, 2, 7], 8)).to eq([])
+    expect(get_indices([8, 8, 8, 8, 8], 8)).to eq([0, 1, 2, 3, 4])
+    expect(get_indices([8, 8, 7, 8, 8], 8)).to eq([0, 1, 3, 4])
+    expect(get_indices([true, false, true, false], true)).to eq([0, 2])
+    expect(get_indices([true, false, true, false], false)).to eq([1, 3])
+  end
+end
