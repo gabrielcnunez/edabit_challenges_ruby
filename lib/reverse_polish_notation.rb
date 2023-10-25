b88ed8ba78d9c0db482865cbf55b8ad4f1ccf690
Gabe Nunez <gabriel.c.nunez@gmail.com>
2023-10-25 11:56:35 -0700

    Solve reverse polish notation challenge

--- lib/reverse_polish_notation.rb
+++ lib/reverse_polish_notation.rb
@@ -12,5 +12,17 @@
 # Learn more about Reverse Polish Notation in Resources.
 
 def rpn(arr)
-  
+  eval_stack = []
+
+  arr.each do |e|
+    if e.is_a?(String)
+      operands = eval_stack.pop(2)
+      result = eval(operands[0].to_s + e + operands[1].to_s)
+      eval_stack << result
+    else
+      eval_stack << e
+    end
+  end
+
+  eval_stack[0]
 end
