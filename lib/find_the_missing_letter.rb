3914761463e94cee212370b99a54f01ec7b1c3cd
Gabe Nunez <gabriel.c.nunez@gmail.com>
2023-10-26 19:31:55 -0700

    Refactor find the missing letter solution for simplicity

--- lib/find_the_missing_letter.rb
+++ lib/find_the_missing_letter.rb
@@ -19,8 +19,5 @@
 # 
 
 def missing_letter(arr)
-  arr.each_cons(2) do |a, b|
-    return (b.ord - 1).chr if b.ord - a.ord != 1
-  end
-  # require 'pry'; binding.pry
+  arr.each_cons(2) { |a, b| return (b.ord - 1).chr if b.ord - a.ord != 1 }
 end
