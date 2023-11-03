cab70cd138463a0b4507f40ee79e1c5bf7e0348f
Gabe Nunez <gabriel.c.nunez@gmail.com>
2023-11-03 12:52:50 -0700

    Solve deadly virus challenge

--- lib/deadly_virus.rb
+++ lib/deadly_virus.rb
@@ -47,5 +47,27 @@
 # N/A
 
 def deadly_virus(persons, num)
-  
+  return persons if num == 0
+
+  current_infections = persons
+  hours_remaining = num
+
+  while hours_remaining > 0
+    new_infections = current_infections.map(&:dup)
+
+    current_infections.each_with_index do |row, index|
+      row.each_with_index do |person, i|
+        if person == 'V'
+          new_infections[index - 1][i] = 'V' if index > 0
+          new_infections[index + 1][i] = 'V' if index + 1 < current_infections.size
+          new_infections[index][i - 1] = 'V' if i > 0
+          new_infections[index][i + 1] = 'V' if i + 1 < row.size
+        end
+      end
+    end
+    current_infections = new_infections
+    hours_remaining -= 1
+  end
+      
+  current_infections
 end
