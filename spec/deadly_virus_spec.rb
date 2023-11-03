4f830bebdb2017ee9d7cddce5926577740ae69c9
Gabe Nunez <gabriel.c.nunez@gmail.com>
2023-11-03 12:54:21 -0700

    Uncomment tests from test suite

--- spec/deadly_virus_spec.rb
+++ spec/deadly_virus_spec.rb
@@ -3,95 +3,95 @@ require './lib/deadly_virus'
 
 RSpec.describe 'Deadly Virus' do
   it 'returns 2-D array of persons affected by virus based on n hours of contact' do
-    # persons=[
-    # ["P","P","P","P","P"],
-    # ["V","P","P","P","P"],
-    # ["P","P","P","P","P"],
-    # ["P","P","P","P","P"],
-    # ["P","P","P","P","P"]
-    # ]
+    persons=[
+    ["P","P","P","P","P"],
+    ["V","P","P","P","P"],
+    ["P","P","P","P","P"],
+    ["P","P","P","P","P"],
+    ["P","P","P","P","P"]
+    ]
 
-    # answer=[
-    # ["P","P","P","P","P"],
-    # ["V","P","P","P","P"],
-    # ["P","P","P","P","P"],
-    # ["P","P","P","P","P"],
-    # ["P","P","P","P","P"]
-    # ]
+    answer=[
+    ["P","P","P","P","P"],
+    ["V","P","P","P","P"],
+    ["P","P","P","P","P"],
+    ["P","P","P","P","P"],
+    ["P","P","P","P","P"]
+    ]
 
-    # expect(deadly_virus(persons,0)).to eq(answer)
+    expect(deadly_virus(persons,0)).to eq(answer)
 
-    # persons=[
-    # ["P","P","P","P","P"],
-    # ["V","P","P","P","P"],
-    # ["P","P","P","P","P"],
-    # ["P","P","P","P","P"],
-    # ["P","P","P","P","P"]
-    # ]
+    persons=[
+    ["P","P","P","P","P"],
+    ["V","P","P","P","P"],
+    ["P","P","P","P","P"],
+    ["P","P","P","P","P"],
+    ["P","P","P","P","P"]
+    ]
 
-    # answer=[
-    # ["V","P","P","P","P"],
-    # ["V","V","P","P","P"],
-    # ["V","P","P","P","P"],
-    # ["P","P","P","P","P"],
-    # ["P","P","P","P","P"]
-    # ]
+    answer=[
+    ["V","P","P","P","P"],
+    ["V","V","P","P","P"],
+    ["V","P","P","P","P"],
+    ["P","P","P","P","P"],
+    ["P","P","P","P","P"]
+    ]
 
-    # expect(deadly_virus(persons,1)).to eq(answer)
+    expect(deadly_virus(persons,1)).to eq(answer)
 
-    # persons=[
-    # ["P","P","P","P","P"],
-    # ["V","P","P","P","P"],
-    # ["P","P","P","P","P"],
-    # ["P","P","P","P","P"],
-    # ["P","P","P","P","P"]
-    # ]
+    persons=[
+    ["P","P","P","P","P"],
+    ["V","P","P","P","P"],
+    ["P","P","P","P","P"],
+    ["P","P","P","P","P"],
+    ["P","P","P","P","P"]
+    ]
 
-    # answer=[
-    # ["V","V","P","P","P"],
-    # ["V","V","V","P","P"],
-    # ["V","V","P","P","P"],
-    # ["V","P","P","P","P"],
-    # ["P","P","P","P","P"]
-    # ]
+    answer=[
+    ["V","V","P","P","P"],
+    ["V","V","V","P","P"],
+    ["V","V","P","P","P"],
+    ["V","P","P","P","P"],
+    ["P","P","P","P","P"]
+    ]
 
-    # expect(deadly_virus(persons,2)).to eq(answer)
+    expect(deadly_virus(persons,2)).to eq(answer)
 
-    # persons=[
-    # ["P","P","P","P","P"],
-    # ["P","P","P","P","P"],
-    # ["P","P","V","P","P"],
-    # ["P","P","P","P","P"],
-    # ["P","P","P","P","P"]
-    # ]
+    persons=[
+    ["P","P","P","P","P"],
+    ["P","P","P","P","P"],
+    ["P","P","V","P","P"],
+    ["P","P","P","P","P"],
+    ["P","P","P","P","P"]
+    ]
 
-    # answer=[
-    # ["P","P","P","P","P"],
-    # ["P","P","V","P","P"],
-    # ["P","V","V","V","P"],
-    # ["P","P","V","P","P"],
-    # ["P","P","P","P","P"]
-    # ]
+    answer=[
+    ["P","P","P","P","P"],
+    ["P","P","V","P","P"],
+    ["P","V","V","V","P"],
+    ["P","P","V","P","P"],
+    ["P","P","P","P","P"]
+    ]
 
-    # expect(deadly_virus(persons,1)).to eq(answer)
+    expect(deadly_virus(persons,1)).to eq(answer)
 
-    # persons=[
-    # ["P","P","P","P","P"],
-    # ["P","P","P","P","P"],
-    # ["P","P","V","P","P"],
-    # ["P","P","P","P","P"],
-    # ["P","P","P","P","P"]
-    # ]
+    persons=[
+    ["P","P","P","P","P"],
+    ["P","P","P","P","P"],
+    ["P","P","V","P","P"],
+    ["P","P","P","P","P"],
+    ["P","P","P","P","P"]
+    ]
 
-    # answer=[
-    # ["P","P","V","P","P"],
-    # ["P","V","V","V","P"],
-    # ["V","V","V","V","V"],
-    # ["P","V","V","V","P"],
-    # ["P","P","V","P","P"]
-    # ]
+    answer=[
+    ["P","P","V","P","P"],
+    ["P","V","V","V","P"],
+    ["V","V","V","V","V"],
+    ["P","V","V","V","P"],
+    ["P","P","V","P","P"]
+    ]
 
-    # expect(deadly_virus(persons,2)).to eq(answer)
+    expect(deadly_virus(persons,2)).to eq(answer)
 
     persons=[
     ["P","P","P","P","P"],
