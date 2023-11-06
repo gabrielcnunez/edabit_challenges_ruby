56f98f848eaf2a1c1a3e820b526e2040fbac29f4
Gabe Nunez <gabriel.c.nunez@gmail.com>
2023-11-05 17:43:09 -0700

    Solve lonely numbers challenge

--- lib/lonely_numbers.rb
+++ lib/lonely_numbers.rb
@@ -19,5 +19,18 @@
 # All tests will include positive integers.
 
 def numbers_need_friends_too(num)
-  
+  num_string = num.to_s
+  friends_string = ''
+
+  num_string.each_char.with_index do |char, i|
+    if i == 0
+      num_string[i + 1] != char ? friends_string += char * 3 : friends_string += char
+    elsif i == num_string.size - 1
+      num_string[i - 1] != char ? friends_string += char * 3 : friends_string += char
+    else
+      num_string[i - 1] != char && num_string [i + 1] != char ? friends_string += char * 3 : friends_string += char
+    end
+  end
+
+  friends_string.to_i
 end
