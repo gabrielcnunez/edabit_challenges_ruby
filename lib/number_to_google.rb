ab7f132f2afe63df77a68bbd28546a1724419982
Gabe Nunez <gabriel.c.nunez@gmail.com>
2023-11-14 12:22:18 -0700

    Solve number to google challenge

--- lib/number_to_google.rb
+++ lib/number_to_google.rb
@@ -27,5 +27,48 @@
 # N/A
 
 def num_to_google(arr)
-  
+  str_arr = arr.map(&:to_s)
+  decrypted_arr = []
+  i = 0
+
+  str_arr.each do |str|
+    next if str.include?('9')
+
+    while i < str.size
+      case str[i]
+      when '1'
+        decrypted_arr << 'g'
+        i += 1
+      when '2'
+        decrypted_arr << 'o'
+        i += 1
+      when '3'
+        decrypted_arr << 'l'
+        i += 1
+      when '4'
+        decrypted_arr << 'e'
+        i += 1
+      when '5'
+        cap_ltr = decrypted_arr.pop.upcase
+        decrypted_arr << cap_ltr
+        i += 1
+      when '6'
+        decrypted_arr << '.'
+        i += 1
+      when '7'
+        decrypted_arr[0].capitalize!
+        i += 1
+      when '8'
+        decrypted_arr.reverse!
+        i += 1
+      when '0'
+        decrypted_arr << decrypted_arr[-1] * (str[i + 1].to_i - 1)
+        i += 2
+      end
+    end
+
+    i = 0
+  end
+
+  decrypted_arr.join
 end
