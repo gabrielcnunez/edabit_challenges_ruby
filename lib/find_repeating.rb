d62f42e787773100c467264bbc3ecc6d6e0e4908
Gabe Nunez <gabriel.c.nunez@gmail.com>
2023-11-27 12:54:11 -0700

    Solve find repeating challenge

--- lib/find_repeating.rb
+++ lib/find_repeating.rb
@@ -22,5 +22,26 @@
 # Non-repeated values should start and end on the same index.
 
 def find_repeating(str)
+  value_groups = []
+  value = nil
+  first_ind = nil
+  last_ind = nil
+  quantity = nil
 
+  str.each_char.with_index do |char, ind|
+    if char != value
+      value_groups << [value, first_ind, last_ind, quantity] if value != nil
+      value = char
+      first_ind = ind
+      last_ind = ind
+      quantity = 1
+    else
+      last_ind = ind
+      quantity += 1
+    end
+
+    value_groups << [value, first_ind, last_ind, quantity] if ind == str.size - 1
+  end
+
+  value_groups
 end
