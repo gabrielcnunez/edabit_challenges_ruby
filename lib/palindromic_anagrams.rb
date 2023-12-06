3ec2830541d275be2f42ed11e8149d0ea4003ab4
Gabe Nunez <gabriel.c.nunez@gmail.com>
2023-12-06 11:07:02 -0700

    Create test for palindromic anagrams challenge

---
+++ lib/palindromic_anagrams.rb
@@ -0,0 +1,21 @@
+# Palindromic Anagrams
+# Given a word, create a function which returns whether or not it's 
+# possible to create a palindrome by rearranging the letters in the word.
+
+# Examples
+# is_palindrome_possible("rearcac") ➞ true
+# # You can make "racecar"
+
+# is_palindrome_possible("suhbeusheff") ➞ true
+# # You can make "sfuehbheufs" (not a real word but still a palindrome)
+
+# is_palindrome_possible("palindrome") ➞ false
+# # It's impossible
+
+# Notes
+# Trivially, words which are already palindromes return true.
+# Words are given in all lowercase.
+
+def is_palindrome_possible(str)
+  
+end
