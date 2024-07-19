8967593aff5d1cc4a792684d6235d1cf952b2777
Gabe Nunez <gabriel.c.nunez@gmail.com>
2024-07-18 19:37:56 -0700

    Create tests for next happy year challenge

---
+++ lib/next_happy_year.rb
@@ -0,0 +1,21 @@
+# Next Happy Year
+# Mubashir needs your help to find out the next happy year.
+
+# A Happy Year is the year with only distinct digits. 
+# Create a function that takes an integer year and returns 
+# the next happy year.
+
+# Examples
+# happy_year(2017) ➞ 2018
+# // 2018 has all distinct digits
+
+# happy_year(1990) ➞ 2013
+
+# happy_year(2021) ➞ 2031
+
+# Notes
+# N/A
+
+def happy_year(int)
+  
+end
---
+++ spec/next_happy_year_spec.rb
@@ -0,0 +1,20 @@
+require 'rspec'
+require './lib/next_happy_year'
+
+RSpec.describe 'Next Happy Year' do
+  it 'takes year as integer, return next year that has unique digits' do
+    expect(happy_year(1990)).to eq(2013)
+    expect(happy_year(2017)).to eq(2018)
+    expect(happy_year(2021)).to eq(2031)
+    expect(happy_year(1001)).to eq(1023)
+    expect(happy_year(1123)).to eq(1203)
+    expect(happy_year(2001)).to eq(2013)
+    expect(happy_year(2334)).to eq(2340)
+    expect(happy_year(3331)).to eq(3401)
+    expect(happy_year(1987)).to eq(2013)
+    expect(happy_year(5555)).to eq(5601)
+    expect(happy_year(7712)).to eq(7801)
+    expect(happy_year(8088)).to eq(8091)
+    expect(happy_year(8999)).to eq(9012)
+  end
+end
