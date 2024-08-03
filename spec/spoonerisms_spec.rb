2bd86e432ff65f01c3ab9746a68bf6a5e22aab21
Gabe Nunez <gabriel.c.nunez@gmail.com>
2024-08-02 19:56:44 -0700

    Uncomment out tests in test suite, solution still passess

--- spec/spoonerisms_spec.rb
+++ spec/spoonerisms_spec.rb
@@ -3,8 +3,8 @@ require './lib/spoonerisms'
 
 RSpec.describe 'Spoonerisms' do
   it 'takes two-word phrase and returns spoonerism of phrase' do
-    # expect(spoonerize("crushing failure")).to eq("fushing crailure")
-    # expect(spoonerize("christmas eve")).to eq("istmas chreve")
+    expect(spoonerize("crushing failure")).to eq("fushing crailure")
+    expect(spoonerize("christmas eve")).to eq("istmas chreve")
     expect(spoonerize("highlighter fluid")).to eq("flighlighter huid")
     expect(spoonerize("chocolate biscuits")).to eq("bocolate chiscuits")
     expect(spoonerize("edabit rules!")).to eq("redabit ules!")
