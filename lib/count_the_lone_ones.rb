a6a00f98e97653425e3521f24e21df829fdbd6e6
Gabe Nunez <gabriel.c.nunez@gmail.com>
2024-08-09 07:58:12 -0700

    Create tests for count the lone ones challenge

---
+++ lib/count_the_lone_ones.rb
@@ -0,0 +1,19 @@
+# Count the Lone Ones
+# Create a function which counts how many lone 1s appear in a given number. 
+# Lone means the number doesn't appear twice or more in a row.
+
+# Examples
+# count_lone_ones(101) ➞ 2
+
+# count_lone_ones(1191) ➞ 1
+
+# count_lone_ones(1111) ➞ 0
+
+# count_lone_ones(462) ➞ 0
+
+# Notes
+# Tests will include positive whole numbers only.
+
+def count_lone_ones(num)
+  
+end
---
+++ spec/count_the_lone_ones.rb
@@ -0,0 +1,16 @@
+require 'rspec'
+require './lib/count_the_lone_ones'
+
+RSpec.describe 'Count the Lone Ones' do
+  it 'takes pos int, returns no. of digits that never appear twice or more in a row' do
+    expect(count_lone_ones(101)).to eq(2)
+    expect(count_lone_ones(1191)).to eq(1)
+    expect(count_lone_ones(1111)).to eq(0)
+    expect(count_lone_ones(11101)).to eq(1)
+    expect(count_lone_ones(462)).to eq(0)
+    expect(count_lone_ones(12131415161718191)).to eq(9)
+    expect(count_lone_ones(11231212111)).to eq(2)
+    expect(count_lone_ones(1)).to eq(1)
+    expect(count_lone_ones(0)).to eq(0)
+  end
+end
