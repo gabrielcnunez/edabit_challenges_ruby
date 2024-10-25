602b1ad2b2987c3698a26df2500ea2efda2d997d
Gabe Nunez <gabriel.c.nunez@gmail.com>
2024-10-25 11:51:29 -0700

    Solve longest slide of pyramid challenge

--- lib/longest_slide_of_pyramid.rb
+++ lib/longest_slide_of_pyramid.rb
@@ -1,11 +1,17 @@
-# Longest Slide of Pyramid
-# Create a function that takes an array of pyramid numbers and returns the maximum
-# sum of consecutive numbers from the top to the bottom of the pyramid.
+# Longest Slide of arr
+# Create a function that takes an array of arr numbers and returns the maximum
+# sum of consecutive numbers from the top to the bottom of the arr.
 
 #                         /3/
 #                         \7\ 4
 #                        2 \4\ 6
 #                       8 5 \9\ 3
+#
+#                         3
+#                        7 4
+#                       2 4 6
+#                      8 5 9 3
+
 
 # # Longest slide down sum is 3 + 7 + 4 + 9 = 23
 # Examples
@@ -19,5 +25,11 @@
 # N/A
 
 def longest_slide(arr)
+  (arr.size - 2).downto(0) do |row|
+    (0...arr[row].size).each do |col|
+      arr[row][col] += [arr[row + 1][col], arr[row + 1][col + 1]].max
+    end
+  end
   
+  arr[0][0]
 end
