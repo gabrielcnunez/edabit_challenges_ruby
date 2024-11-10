126ffbd38ccb474fcb00cdbb69936bfd9dc1222a
Gabe Nunez <gabriel.c.nunez@gmail.com>
2024-11-10 12:13:30 -0700

    Solve possible palindrome challenge

--- lib/possible_palindromes.rb
+++ lib/possible_palindromes.rb
@@ -17,5 +17,9 @@
 # N/A
 
 def possible_palindrome(str)
-  
+  odd_chars = 0
+  char_values = str.chars.tally.values
+  char_values.each { |value| odd_chars += 1 if value.odd? }
+
+  odd_chars <= 1
 end
