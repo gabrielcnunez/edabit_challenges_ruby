777c4338eb10d818b9fa8e18b3d8c18e1efe2d32
Gabe Nunez <gabriel.c.nunez@gmail.com>
2024-11-14 19:11:44 -0700

    Uncomment out tests, solution still passing

--- spec/character_titles_spec.rb
+++ spec/character_titles_spec.rb
@@ -3,15 +3,15 @@ require './lib/character_titles'
 
 RSpec.describe 'Game of Thrones: Character Titles' do
   it 'takes string, returns string with correct case for character titles' do
-    # expect(correct_title("lord eddard stark, hand of the king.")).to eq("Lord Eddard Stark, Hand of the King.")
-    # expect(correct_title("jaime lannister, lord commander of the kingsguard.")).to eq("Jaime Lannister, Lord Commander of the Kingsguard.")
-    # expect(correct_title("varys, master of whisperers.")).to eq("Varys, Master of Whisperers.")
-    # expect(correct_title("doran martell, prince of dorne, lord of sunspear.")).to eq("Doran Martell, Prince of Dorne, Lord of Sunspear.")
+    expect(correct_title("lord eddard stark, hand of the king.")).to eq("Lord Eddard Stark, Hand of the King.")
+    expect(correct_title("jaime lannister, lord commander of the kingsguard.")).to eq("Jaime Lannister, Lord Commander of the Kingsguard.")
+    expect(correct_title("varys, master of whisperers.")).to eq("Varys, Master of Whisperers.")
+    expect(correct_title("doran martell, prince of dorne, lord of sunspear.")).to eq("Doran Martell, Prince of Dorne, Lord of Sunspear.")
 
-    # expect(correct_title("TYRION LANNISTER, HAND OF THE QUEEN.")).to eq("Tyrion Lannister, Hand of the Queen.")
-    # expect(correct_title("GRAND MAESTER PYCELLE.")).to eq("Grand Maester Pycelle.")
-    # expect(correct_title("EURON GREYJOY, KING OF THE IRON ISLANDS, LORD REAPER OF PYKE.")).to eq("Euron Greyjoy, King of the Iron Islands, Lord Reaper of Pyke.")
-    # expect(correct_title("PETYR BAELISH, LORD PROTECTOR OF THE VALE.")).to eq("Petyr Baelish, Lord Protector of the Vale.")
+    expect(correct_title("TYRION LANNISTER, HAND OF THE QUEEN.")).to eq("Tyrion Lannister, Hand of the Queen.")
+    expect(correct_title("GRAND MAESTER PYCELLE.")).to eq("Grand Maester Pycelle.")
+    expect(correct_title("EURON GREYJOY, KING OF THE IRON ISLANDS, LORD REAPER OF PYKE.")).to eq("Euron Greyjoy, King of the Iron Islands, Lord Reaper of Pyke.")
+    expect(correct_title("PETYR BAELISH, LORD PROTECTOR OF THE VALE.")).to eq("Petyr Baelish, Lord Protector of the Vale.")
     expect(correct_title("MANCE RAYDER, KING-BEYOND-THE-WALL.")).to eq("Mance Rayder, King-Beyond-the-Wall.")
 
     expect(correct_title("jOn SnoW, kINg IN thE noRth.")).to eq("Jon Snow, King in the North.")
